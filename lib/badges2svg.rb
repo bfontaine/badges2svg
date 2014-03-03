8e7892a66a9d3da9d43ffcc315e89b6d35a916ab
Baptiste Fontaine <batifon@yahoo.fr>
2014-03-04 00:24:36 +0100

    #compile_pattern to have clean replacement rules

--- lib/badges2svg.rb
+++ lib/badges2svg.rb
@@ -1,8 +1,21 @@
 module BadgesToSVG
   class << self
 
+    ROOT = 'https://img.shields.io'
+
+    RULES = {
+      :travis => [
+        'https?://travis-ci\\.org/%{user}/%{repo}\\.png',
+        '/travis/%{user}/%{repo}.svg'
+      ]
+    }
+
     def version
       '0.0.1'
+    end
+
+    def compile_pattern pat
+      Regexp.new ("^#{pat.gsub(/%\{(\w+)\}/) { "(?<#{$1}>.+?)" }}$")
     end
 
     def replace(content)
--- tests/tests.rb
+++ tests/tests.rb
@@ -26,11 +26,39 @@ class BadgesToSVGTests < Test::Unit::TestCase
     assert(BadgesToSVG.version =~ /^\d+\.\d+\.\d+/)
   end
 
-  def test_empty_file
+  # == BadgesToSVG#compile_pattern == #
+
+  def test_compile_pattern_empty
+    assert_equal(/^$/, BadgesToSVG.compile_pattern(""))
+  end
+
+  def test_compile_pattern_no_special_chars
+    assert_equal(/^foobar$/, BadgesToSVG.compile_pattern("foobar"))
+    assert_equal(/^q-a yo$/, BadgesToSVG.compile_pattern("q-a yo"))
+  end
+
+  def test_compile_pattern_with_special_chars
+    assert_equal(/^foo.bar$/, BadgesToSVG.compile_pattern("foo.bar"))
+    assert_equal(/^a?$/, BadgesToSVG.compile_pattern("a?"))
+  end
+
+  def test_compile_pattern_with_one_field_name
+    assert_equal(/^(?<abc>.+?)$/, BadgesToSVG.compile_pattern("%{abc}"))
+    assert_equal(/^a?$/, BadgesToSVG.compile_pattern("a?"))
+  end
+
+  def test_compile_pattern_with_multiple_field_names
+    assert_equal(/^(?<a>.+?)\/(?<b>.+?)$/,
+                 BadgesToSVG.compile_pattern("%{a}/%{b}"))
+  end
+
+  # == BadgesToSVG#replace == #
+
+  def test_replace_empty_file
     assert_equal("", BadgesToSVG.replace(""))
   end
 
-  def test_simple_file
+  def test_replace_simple_file
     ct = "# README\n\nHello World!"
    assert_equal(ct, BadgesToSVG.replace(ct))
   end
